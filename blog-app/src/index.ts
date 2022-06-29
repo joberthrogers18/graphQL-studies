b20a2d4537ac5532a2bff45294e94f66f7fa13bd
Joberth Rogers <joberth.rogers18@gmail.com>
2022-06-28 23:39:09 -0300

    feat: create a skeleton of mutations

--- blog-app/src/index.ts
+++ blog-app/src/index.ts
@@ -1,12 +1,22 @@
-import {ApolloServer} from 'apollo-server';
-import {typeDefs} from './schema';
-import {resolvers} from './resolvers'
+import { ApolloServer } from 'apollo-server';
+import { typeDefs } from './schema';
+import { resolvers } from './resolvers';
+import { PrismaClient, Prisma } from '@prisma/client';
+
+const prisma = new PrismaClient()
+
+export interface Context {
+    prisma: PrismaClient<Prisma.PrismaClientOptions, never, Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined>
+}
 
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: {
+        prisma
+    }
 });
 
 server.listen().then(({ url }) => {
-    console.log(`Server running on ${url}`); 
+    console.log(`Server running on ${url}`);
 })
