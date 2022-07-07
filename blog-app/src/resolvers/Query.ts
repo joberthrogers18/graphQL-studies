3af779f8d0d3c1087b6448ffc5d588da94ef4b76
Joberth Rogers <joberth.rogers18@gmail.com>
2022-07-06 23:59:43 -0300

    feat: create query to get me data

--- blog-app/src/resolvers/Query.ts
+++ blog-app/src/resolvers/Query.ts
@@ -1,7 +1,32 @@
-import { Post } from "@prisma/client";
+import { Post, User } from "@prisma/client";
 import { Context } from "..";
 
 export const Query = {
+    me: (
+        _: any,
+        __: any,
+        { prisma, userInfo }: Context
+    ) => {
+        if (!userInfo) return null;
+        return prisma.user.findUnique({
+            where: {
+                id: Number(userInfo.userId)
+            }
+        })
+    },
+
+    profile: (
+        _: any,
+        { userId }: { userId: string },
+        { prisma }: Context
+    ) => {
+        return prisma.profile.findUnique({
+            where: {
+                id: Number(userId),
+            }
+        })
+    },
+
     posts: (_: any, __: any, { prisma }: Context): Promise<Post[]> => {
         return prisma.post.findMany({
             orderBy: [
