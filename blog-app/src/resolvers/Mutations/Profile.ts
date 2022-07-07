f8a3a17468dec8f1d5ca5595ceff6cd73519beaf
Joberth Rogers <joberth.rogers18@gmail.com>
2022-07-07 00:37:38 -0300

    feat: create a resolver to get user from profile

---
+++ blog-app/src/resolvers/Mutations/Profile.ts
@@ -0,0 +1,22 @@
+import { Post, User } from "@prisma/client";
+import { Context } from "../..";
+
+interface ProfileParentType {
+    id: number;
+    bio: string;
+    userId: number;
+}
+
+export const Profile = {
+    user: (
+        parent: ProfileParentType,
+        __: any,
+        { prisma, userInfo }: Context
+    ) => {
+        return prisma.user.findUnique({
+            where: {
+                id: parent.userId
+            }
+        });
+    },
+}
--- blog-app/src/resolvers/index.ts
+++ blog-app/src/resolvers/index.ts
@@ -1,7 +1,9 @@
 import { Query } from "./Query"
 import { Mutation } from "./Mutations/Mutation"
+import { Profile } from "./Mutations/Profile";
 
 export const resolvers = {
     Query,
     Mutation,
+    Profile,
 }
