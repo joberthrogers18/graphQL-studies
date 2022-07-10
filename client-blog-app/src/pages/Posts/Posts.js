bd1e775a4cd9b0fc28390e133980c982f50ee0e2
Joberth Rogers <joberth.rogers18@gmail.com>
2022-07-10 19:07:15 -0300

    feat: query posts from database

--- client-blog-app/src/pages/Posts/Posts.js
+++ client-blog-app/src/pages/Posts/Posts.js
@@ -1,6 +1,47 @@
 import React from "react";
 import Post from "../../components/Post/Post";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_POSTS = gql`
+  query {
+    posts {
+      id
+      title
+      content
+      createdAt
+      user {
+        name
+      }
+    }
+  }
+`;
 
 export default function Posts() {
-  return <div></div>;
+  const { data, error, loading } = useQuery(GET_POSTS);
+
+  console.log({
+    data,
+    error,
+    loading,
+  });
+
+  if (error) return <div>Error Page</div>;
+
+  if (loading) return <div>Loading...</div>;
+
+  const { posts } = data;
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <Post
+          title={post.title}
+          content={post.content}
+          date={post.createdAt}
+          id={post.id}
+          user={post.user.name}
+        />
+      ))}
+    </div>
+  );
 }
